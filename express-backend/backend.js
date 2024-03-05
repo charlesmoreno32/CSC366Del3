5ee9fcbff727794c2447fb32bbbff036677c81b5
Charles Moreno <93685786+charlesmoreno32@users.noreply.github.com>
2024-03-04 21:56:22 -0800

    backend good

--- express-backend/backend.js
+++ express-backend/backend.js
@@ -9,6 +9,10 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
+app.post('/createTables', (req, res) => {
+
+});
+
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 }); 
